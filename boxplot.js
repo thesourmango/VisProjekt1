9dcc129245223d092327faf76de0b478e9b58c3d
thesourmango <juhuuuuu@hotmail.com>
2020-05-18 13:19:28 +0300

    Read temp data

--- boxplot.js
+++ boxplot.js
@@ -1,3 +1,21 @@
 function drawBoxplot() {
+     // Läs in extern JSON data (d3.v5!)
+     d3.json("boxplot.json").then( function(jsonData) {
+
+        // Definiera variabler
+        var width = 600, height = 300, margin = 30;
+        var chartWidth = width - (margin*2);
+        var chartHeight = height - (margin*2);
     
-}
+        // Ladda in datan
+        var temps = [];
+        for (i=0; i<jsonData.temperatures.length; i++) {
+            temps.push(jsonData.temperatures[i].temp);
+        }
+        console.log(temps);
+
+        
+
+
+     });
+};

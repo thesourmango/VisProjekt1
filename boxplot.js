8155c87d2988ea6f2543713a0812b9ecb1de10d0
thesourmango <juhuuuuu@hotmail.com>
2020-05-22 10:58:55 +0300

    Finished Box, Added Force

--- boxplot.js
+++ boxplot.js
@@ -3,9 +3,11 @@ function drawBoxplot() {
      d3.json("boxplot.json").then( function(jsonData) {
 
         // Definiera variabler
-        var width = 600, height = 300, margin = 30;
+        var width = 600, height = 150, margin = 30;
         var chartWidth = width - (margin*2);
         var chartHeight = height - (margin*2);
+        var boxHeight = 50, boxPosition = chartHeight-boxHeight;
+        var lineY = boxPosition + boxHeight / 2;
     
         // Ladda in datan
         var temps = [];
@@ -33,12 +35,35 @@ function drawBoxplot() {
 
         // Skapa en grupp som inte täcker hela ritunderlaget (använd margins!)
         var chartGroup = canvas.append("g").attr("transform","translate(0,0)");
-
+        
+        // Rita en linje från min till maxvärdet
+        chartGroup.append("line").attr("stroke", "black")
+            .attr("x1", xScale(min)).attr("x2", xScale(max))
+            .attr("y1", lineY).attr("y2", lineY);
+        
         chartGroup.append("rect")
-            .attr("width", function(data) { return xScale(uq) - xScale(lq) } )
-            .attr("height", 50)
+            .attr("width", xScale(uq) - xScale(lq) )
+            .attr("height", boxHeight)
+            .attr("fill", "grey")
+            .attr("stroke", "black")
             .attr("x", xScale(lq) )
-            .attr("y", 20);
+            .attr("y", boxPosition);
+        
+        // Rita en vertikal linje vid minsta värdet
+        chartGroup.append("line").attr("stroke", "black")
+        .attr("x1", xScale(min)).attr("x2", xScale(min))
+        .attr("y1", boxPosition + 10).attr("y2", boxPosition + boxHeight - 10);
+
+        // Rita en vertikal linje vid minsta värdet
+        chartGroup.append("line").attr("stroke", "black")
+        .attr("x1", xScale(max)).attr("x2", xScale(max))
+        .attr("y1", boxPosition + 10).attr("y2", boxPosition + boxHeight - 10);
+
+        // Rita en vertikal linje vid Medianen
+        chartGroup.append("line").attr("stroke", "black")
+        .attr("x1", xScale(median)).attr("x2", xScale(median))
+        .attr("y1", boxPosition).attr("y2", boxPosition + boxHeight);
+
 
         // Rita ut axeln
         chartGroup.append("g").call(xAxis);
---
+++ force.js
@@ -0,0 +1,11 @@
+function drawForce() {
+    d3.json("nodes.json", function (jsonData) {
+
+        var width = 400, height = 400;
+
+        var canvas = d3.select("body").append("svg").attr("width",width).attr("height",height);
+
+        
+
+    });
+};
